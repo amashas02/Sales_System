905df53e6cf9579a830c1c5c7c2bfc80ced5abf3
amashas02 <amashasenanayaka@gmail.com>
2026-02-19 23:50:52 +0530

    Database: Expanded product table with buy price, Category, and available quantity removed barcode

--- src/sales_system/Products.java
+++ src/sales_system/Products.java
@@ -96,6 +96,8 @@ public class Products extends javax.swing.JFrame {
         }
         //</editor-fold>
 
+        
+        //databse
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
