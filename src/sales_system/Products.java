780aa1774e9bd2d15d7bb01fc6ebd16ff3c85fb0
amashas02 <amashasenanayaka@gmail.com>
2026-02-19 14:25:08 +0530

    UI: Added Back button functionality to Sales page

--- src/sales_system/Products.java
+++ src/sales_system/Products.java
@@ -48,6 +48,11 @@ public class Products extends javax.swing.JFrame {
         jLabel1.setText("Products");
 
         jButton1.setText("Back");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jLabel2.setText("jLabel2");
 
@@ -157,6 +162,12 @@ public class Products extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    // Navigation back to Dashboard
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
